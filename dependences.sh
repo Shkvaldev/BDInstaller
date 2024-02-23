ff0bb5393997c9465f09032119a6d177b2198b2a
ZaShkval <shkvalgrozny@gmail.com>
2024-02-23 14:50:38 +0500

    update Brutal DOOM v22 test 3

--- dependences.sh
+++ dependences.sh
@@ -1,22 +1,3 @@
-if_util_exists() {
-	if ! [ -x "$(command -v $1)" ]; then
-	  echo 1;
-	  return;
-	fi
-	echo 0;
-}
-
-if [ $(if_util_exists "apt-get") -eq 0 ]; then
-	sudo apt-get update -y;
-	sudo apt-get install fluidsynth libsdl2-dev unrar unzip aria2;
-elif [ $(if_util_exists "pacman") -eq 0 ]; then
-	sudo pacman -Sy fluidsynth sdl2 gtk3 unrar unzip aria2;
-else
-	dialog --title "[ Brutal DOOM Installer ]" --msgbox "It seems that you are running unsupported OS - check BDInstaller's repo!" 10 40;
-	exit 1;
-fi
-
-dialog --title "[ Brutal DOOM Installer ]" --infobox "Alright, all dependences are installed :)" 10 40;
-sleep 2;
-clear;
-exit 0;
+version https://git-lfs.github.com/spec/v1
+oid sha256:d88c9cc57184e0daa5ccf1ed099b96ced08e9267a84495f69331c7858a481fc3
+size 617
--- uninstall.sh
+++ uninstall.sh
@@ -1,32 +1,3 @@
-# UI
-start_uninstall() {
-    dialog --title "[ Brutal DOOM Uninstaller ]" --yesno "Are you sure, that you want to uninstall Brutal DOOM?" 10 40;
-	result=$?;
-	if [ $result -eq 1 ]; then
-		clear;
-		exit 0;
-	fi
-	clear;
-	warning_about_saves;
-}
-
-warning_about_saves() {
-    dialog --title "[ Brutal DOOM Uninstaller ]" --yesno "WARNING! This program will COMPLETELY erase Brutal DOOM itself with it's configuration\nAll saves will be lost!" 10 60;
-	result=$?;
-	if [ $result -eq 1 ]; then
-		clear;
-		exit 0;
-	fi
-	clear;
-	deleting_game_files;
-}
-
-deleting_game_files() {
-    dialog --title "[ Brutal DOOM Uninstaller ]" --infobox "Deleting Brutal DOOM files ..." 10 60;
-    sleep 1;
-    rm -rf /usr/share/games/brutal-doom /usr/share/applications/BrutalDOOM.desktop
-	dialog --title "[ Brutal DOOM Uninstaller ]" --msgbox "Deleting Brutal DOOM has been completed!" 10 60;
-	clear;
-	exit 0;
-}
-start_uninstall;
+version https://git-lfs.github.com/spec/v1
+oid sha256:bcc04ee9c0b00fd669476be159722a2923df7acb05fd2f3d3fdc6890c9152846
+size 898
